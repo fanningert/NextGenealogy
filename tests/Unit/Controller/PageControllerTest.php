706943a365cdf682991cd6597115ca1e4c05be1e
Thomas Fanninger <thomas@fanninger.at>
2019-08-27 21:37:25 +0200

    Create skeleton app

---
+++ tests/Unit/Controller/PageControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\NextGenealogy\Tests\Unit\Controller;
+
+use PHPUnit_Framework_TestCase;
+
+use OCP\AppFramework\Http\TemplateResponse;
+
+use OCA\NextGenealogy\Controller\PageController;
+
+
+class PageControllerTest extends PHPUnit_Framework_TestCase {
+	private $controller;
+	private $userId = 'john';
+
+	public function setUp() {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+
+		$this->controller = new PageController(
+			'nextgenealogy', $request, $this->userId
+		);
+	}
+
+	public function testIndex() {
+		$result = $this->controller->index();
+
+		$this->assertEquals('index', $result->getTemplateName());
+		$this->assertTrue($result instanceof TemplateResponse);
+	}
+
+}
